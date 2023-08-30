02b6fd5447eaaffe43fa62f2e31ce6347e859a5e
Gabriel Y. Honji <gabriel.honji78@gmail.com>
2023-08-30 11:05:27 -0300

    3/4 done

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,3 +4,11 @@ $(function () {
         $scroll.toggleClass('scrolled', $(this).scrollTop() > $scroll.height() * 2);
     });
 });
+
+// Carousel element js
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
